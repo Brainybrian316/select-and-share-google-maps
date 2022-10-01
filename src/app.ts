22a1d18734e9153d68d1a73d7c7e0d0eaec348c2
Brainybrian316 <brainybrian316@gmail.com>
2022-10-01 16:46:54 -0400

    set up google api key for geolocation

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-import 'dotenv/config';
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+console.log(process.env);
 import axios from 'axios';
 
 const form = document.querySelector('form')! as HTMLFormElement;

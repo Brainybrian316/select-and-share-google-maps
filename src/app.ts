551ca99be54832baae9cf8395f53c96fdf06c189
Brainybrian316 <brainybrian316@gmail.com>
2022-10-01 16:07:39 -0400

    set up for api request to google

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+const form = document.querySelector('form')! as HTMLFormElement;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+	event.preventDefault();
+
+	const enteredAddresst = addressInput.value;
+
+	//send to gogle api
+}
+
+form.addEventListener('submit');

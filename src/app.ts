839725183498298c02d651157c707d1c219ecc56
Brainybrian316 <brainybrian316@gmail.com>
2022-10-01 16:25:49 -0400

    set up google api key for geolocation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
+import 'dotenv/config';
+import axios from 'axios';
+
 const form = document.querySelector('form')! as HTMLFormElement;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+const apiKey = process.env.API_KEY;
 
 function searchAddressHandler(event: Event) {
 	event.preventDefault();
 
-	const enteredAddresst = addressInput.value;
+	const enteredAddress = addressInput.value;
 
-	//send to gogle api
+	axios
+		.get(
+			`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+				enteredAddress,
+			)}&key=${apiKey}`,
+		)
+		.then((response) => {
+			console.log(response);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 }
 
-form.addEventListener('submit');
+form.addEventListener('submit', searchAddressHandler);
